b35e1e8f649125d580640d3f6432829ebdb84858
sbudumyan <s.budumyan@gmail.com>
2017-02-25 21:21:05 +0300

    Create model , migration and seeder for : user_types - db table

---
+++ database/seeds/UserTypesTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTypesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('user_types')->insert([
+            'type' => 'root',
+            'desc' => '',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('user_types')->insert([
+            'type' => 'manager',
+            'desc' => '',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+        DB::table('user_types')->insert([
+            'type' => 'driver',
+            'desc' => '',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]);
+
+    }
+}
+
+
+
+
