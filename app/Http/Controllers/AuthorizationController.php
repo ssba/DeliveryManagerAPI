9fc29767700d10cafa5002d56403ab6004c60766
sbudumyan <s.budumyan@gmail.com>
2017-03-05 04:30:12 +0300

    Work w/ AuthorizationController

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -3,23 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use Hash;
+use Illuminate\Validation\ValidationException;
+use App\User;
 
 class AuthorizationController extends Controller
 {
+    public function getToken(Request $request)
+    {
 
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator->errors()->all());
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if (Hash::check($request->password, $user->password))
+        {
+            return $user;
+        }
+
+
+
+        return 'err';
+    }
+
+    public function renewToken(Request $request)
+    {
+
+    }
+
+    public function unsetToken(Request $request)
+    {
+
+    }
 }
-
-/**
- * all POST /login?email=email&password=password Запрос токена с валидным временем 20 минут
- */
-Route::post('/login', ['as' => 'GetTokenByCredentials', 'uses' => 'AuthorizationController@getToken', 'role' => '*']);
-
-/**
- *  all PUT /login?token=token Обновление времени валидности токена (с момента запроса + 20 минут) = ДЛЯ всех Валидных токенов
- */
-Route::put('/login', ['as' => 'RenewToken', 'uses' => 'AuthorizationController@renewToken', 'role' => '*']);
-
-/**
- * all DELETE /logout?token=token Уничтожение токена
- */
-Route::delete('/logout', ['as' => 'UnsetToken', 'uses' => 'AuthorizationController@unsetToken', 'role' => '*']);
