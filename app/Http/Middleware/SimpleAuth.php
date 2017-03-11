24ec495a29430ac80fc98d06c142c7656cb2ee5d
sbudumyan <s.budumyan@gmail.com>
2017-03-11 23:39:17 +0300

    Create SimpleAuth middleware

---
+++ app/Http/Middleware/SimpleAuth.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Route;
+use Cache;
+use App\User;
+use Illuminate\Auth\Access\AuthorizationException;
+
+class SimpleAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $routeRole = Route::getCurrentRoute()->action['role'];
+        if(!is_array($routeRole))
+            $routeRole = [$routeRole];
+
+        if (in_array('*', $routeRole))
+            return $next($request);
+
+        try{
+            $session = Cache::get("user.session.".$request->token);
+
+            if (is_null($session))
+                throw new \Exception();
+
+            $user = User::where('guid', $session['user'])->firstOrFail();
+            $userRole = $user->role->type;
+
+            if($userRole == $routeRole || $userRole == "root")
+                return $next($request);
+
+        } catch (\Exception $e){
+            throw new AuthorizationException();
+        }
+
+
+
+    }
+}
