0199ddddc78d6f57eb1855c0e60a49319b3cb40b
sbudumyan <s.budumyan@gmail.com>
2017-04-04 04:51:44 +0300

    Add Output middleware to Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'output' => \App\Http\Middleware\Output::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
