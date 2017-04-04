69985c32fb8964a5ca95adcbc4553d7d080a28fc
sbudumyan <s.budumyan@gmail.com>
2017-04-04 04:53:33 +0300

    Implemenet Controllers

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,32 +3,117 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use DB;
+use Webpatser\Uuid\Uuid;
+use App\Product;
 
 class ProductController extends Controller
 {
-    public function getAll()
+    public function getAll() : array
     {
+        $data = [];
+        $products = Product::all();
 
+        foreach ($products as $key => $product){
+            $data[] =[
+                "type" => "products",
+                "id" => $product->guid,
+                "attributes" => $product,
+                "links" => [
+                    "self" => route('productsGetSingleProduct', ['productGUID' => $product->guid])
+                ]
+            ];
+        }
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('productsGetAllProducts')
+            ]
+        ];
+
+        return $json;
     }
 
-    public function getSingle(string $userGUID)
+    public function getSingle(string $productGUID) : array
     {
+        $product = Product::where('guid', $productGUID)->firstOrFail();
+        $data =[
+            "type" => "products",
+            "id" => $product->guid,
+            "attributes" => $product
+        ];
 
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('productsGetSingleProduct', ['productGUID' => $product->guid])
+            ]
+        ];
+
+        return $json;
     }
 
-    public function createSingle(Request $request)
+    public function createSingle(Request $request) : array
     {
+        $newProductGUID = (string)Uuid::generate(4);
 
+        $attributes = [
+            "sku" => $request->sku,
+            "name" => $request->name,
+            "width" => $request->width,
+            "height" => $request->height,
+            "length" => $request->length,
+            "volume" => $request->volume,
+            "weight" => $request->weight,
+            "image" => $request->image
+        ];
+
+        DB::transaction(function () use ($request, $attributes, $newProductGUID) {
+
+            $attributes['guid'] = $newProductGUID;
+
+            $status = new Product($attributes);
+
+            if(!$status->save())
+                throw new ValidationException($status->errors());
+
+        });
+
+        return $this->getSingle($newProductGUID);
     }
 
-    public function updateSingle(string $userGUID, Request $request)
+    public function updateSingle(string $productGUID, Request $request) : array
     {
+        $product = Product::where('guid', $productGUID)->firstOrFail();
+        $productRequest = $request->only('sku', 'name', 'width', 'height', 'length', 'volume', 'weight', 'image');
 
+        if (empty($productRequest))
+            return $this->getSingle($productGUID);
+
+        DB::transaction(function () use ($request, $product, $productRequest) {
+
+            $product_update = $product->update($productRequest);
+
+            if (!$product_update)
+                throw new ValidationException($product_update->errors());
+
+        });
+
+        return $this->getSingle($productGUID);
     }
 
-    public function deleteSingle(string $userGUID)
+    public function deleteSingle(string $productGUID) : array
     {
+        $product = $this->getSingle($productGUID);
 
+        DB::transaction(function () use ($productGUID) {
+            Product::where('guid', $productGUID)->delete();
+        });
+
+        return $product;
     }
 
 }
--- app/Http/Controllers/UserTypeController.php
+++ app/Http/Controllers/UserTypeController.php
@@ -3,32 +3,111 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use DB;
+use Webpatser\Uuid\Uuid;
+use App\UserType;
 
 class UserTypeController extends Controller
 {
-    public function getAll()
+    public function getAll() : array
     {
+        $data = [];
+        $UserTypes = UserType::all();
 
+        foreach ($UserTypes as $key => $UserType){
+            $data[] =[
+                "type" => "user_types",
+                "id" => $UserType->guid,
+                "attributes" => $UserType,
+                "links" => [
+                    "self" => route('user_typesGetSingleUsersType', ['typeGUID' => $UserType->guid])
+                ]
+            ];
+        }
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('user_typesGetAllUsersTypes')
+            ]
+        ];
+
+        return $json;
     }
 
-    public function getSingle(string $typeGUID)
+    public function getSingle(string $typeGUID) : array
     {
+        $UserType = UserType::where('guid', $typeGUID)->firstOrFail();
+        $data =[
+            "type" => "user_types",
+            "id" => $UserType->guid,
+            "attributes" => $UserType
+        ];
 
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('user_typesGetSingleUsersType', ['typeGUID' => $UserType->guid])
+            ]
+        ];
+
+        return $json;
     }
 
-    public function createSingle(Request $request)
+    public function createSingle(Request $request) : array
     {
+        $newUserTypeGUID = (string)Uuid::generate(4);
 
+        $attributes = [
+            "type" => $request->type,
+            "desc" => $request->desc,
+        ];
+
+        DB::transaction(function () use ($request, $attributes, $newUserTypeGUID) {
+
+            $attributes['guid'] = $newUserTypeGUID;
+
+            $status = new UserType($attributes);
+
+            if(!$status->save())
+                throw new ValidationException($status->errors());
+
+        });
+
+        return $this->getSingle($newUserTypeGUID);
     }
 
-    public function updateSingle(string $typeGUID, Request $request)
+    public function updateSingle(string $typeGUID, Request $request) : array
     {
+        $UserType = UserType::where('guid', $typeGUID)->firstOrFail();
+        $UserTypeRequest = $request->only('type', 'desc');
 
+        if (empty($UserTypeRequest))
+            return $this->getSingle($UserType);
+
+        DB::transaction(function () use ($request, $UserType, $UserTypeRequest) {
+
+            $UserType_update = $UserType->update($UserTypeRequest);
+
+            if (!$UserType_update)
+                throw new ValidationException($UserType_update->errors());
+
+        });
+
+        return $this->getSingle($typeGUID);
     }
 
-    public function deleteSingle(string $typeGUID)
+    public function deleteSingle(string $typeGUID) : array
     {
+        $UserType = $this->getSingle($typeGUID);
 
+        DB::transaction(function () use ($typeGUID) {
+            UserType::where('guid', $typeGUID)->delete();
+        });
+
+        return $UserType;
     }
 
 }
