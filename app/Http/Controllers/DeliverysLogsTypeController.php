c90bcfc60fa8596c4597bf4d6f3ada4453bfa4f7
sbudumyan <s.budumyan@gmail.com>
2017-04-17 00:39:05 +0300

    Implemenet Controllers

--- app/Http/Controllers/DeliverysLogsTypeController.php
+++ app/Http/Controllers/DeliverysLogsTypeController.php
@@ -3,32 +3,111 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use DB;
+use Webpatser\Uuid\Uuid;
+use App\RouteLogType;
 
 class DeliverysLogsTypeController extends Controller
 {
-    public function getAll()
+    public function getAll() : array
     {
+        $data = [];
+        $RouteLogTypes = RouteLogType::all();
 
+        foreach ($RouteLogTypes as $key => $RouteLogType){
+            $data[] =[
+                "type" => "route_logs_type",
+                "id" => $RouteLogType->guid,
+                "attributes" => $RouteLogType,
+                "links" => [
+                    "self" => route('route_logs_typeGetSingleDeliverysLogsType', ['typeGUID' => $RouteLogType->guid])
+                ]
+            ];
+        }
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('route_logs_typeGetAllDeliverysLogsTypes')
+            ]
+        ];
+
+        return $json;
     }
 
-    public function getSingle(string $typeGUID)
+    public function getSingle(string $typeGUID) : array
     {
+        $RouteLogType = RouteLogType::where('guid', $typeGUID)->firstOrFail();
+        $data =[
+            "type" => "user_types",
+            "id" => $RouteLogType->guid,
+            "attributes" => $RouteLogType
+        ];
 
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('$RouteLogType', ['typeGUID' => $RouteLogType->guid])
+            ]
+        ];
+
+        return $json;
     }
 
-    public function createSingle(Request $request)
+    public function createSingle(Request $request) : array
     {
+        $newRouteLogTypeGUID = (string)Uuid::generate(4);
 
+        $attributes = [
+            "type" => $request->type,
+            "desc" => $request->desc,
+        ];
+
+        DB::transaction(function () use ($request, $attributes, $newRouteLogTypeGUID) {
+
+            $attributes['guid'] = $newRouteLogTypeGUID;
+
+            $status = new RouteLogType($attributes);
+
+            if(!$status->save())
+                throw new ValidationException($status->errors());
+
+        });
+
+        return $this->getSingle($newRouteLogTypeGUID);
     }
 
-    public function updateSingle(string $typeGUID, Request $request)
+    public function updateSingle(string $typeGUID, Request $request) : array
     {
+        $RouteLogType = RouteLogType::where('guid', $typeGUID)->firstOrFail();
+        $RouteLogTypeRequest = $request->only('type', 'desc');
 
+        if (empty($RouteLogTypeRequest))
+            return $this->getSingle($typeGUID);
+
+        DB::transaction(function () use ($request, $RouteLogType, $RouteLogTypeRequest) {
+
+            $RouteLogType_update = $RouteLogType->update($RouteLogTypeRequest);
+
+            if (!$RouteLogType_update)
+                throw new ValidationException($RouteLogType_update->errors());
+
+        });
+
+        return $this->getSingle($typeGUID);
     }
 
-    public function deleteSingle(string $typeGUID)
+    public function deleteSingle(string $typeGUID) : array
     {
+        $RouteLogType = $this->getSingle($typeGUID);
 
+        DB::transaction(function () use ($typeGUID) {
+            RouteLogType::where('guid', $typeGUID)->delete();
+        });
+
+        return $RouteLogType;
     }
 
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,32 +3,141 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use DB;
+use Webpatser\Uuid\Uuid;
+use App\User;
+use App\UserType;
 
 class UserController extends Controller
 {
-    public function getAll()
+    public function getAll() : array
     {
+        $data = [];
+        $Users = User::all();
 
+        foreach ($Users as $key => $User){
+            $data[] =[
+                "type" => "users",
+                "id" => $User->guid,
+                "attributes" => $User,
+                "relationships" => [
+                    "role" => [
+                        "links" => [
+                            "self" => route('user_typesGetSingleUsersType', ['typeGUID' => $User->role->guid])
+                        ],
+                        "data" => [
+                            "type" => "user_types",
+                            "guid" => $User->role->guid,
+                            "attributes" => $User->role
+                        ],
+                    ],
+                ],
+                "links" => [
+                    "self" => route('userGetSingleUser', ['userGUID' => $User->guid])
+                ]
+            ];
+        }
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('userGetAllUsers')
+            ]
+        ];
+        return $json;
     }
 
-    public function getSingle(string $userGUID)
+    public function getSingle(string $userGUID) : array
     {
+        $User = User::where('guid', $userGUID)->firstOrFail();
 
+        $data =[
+            "type" => "users",
+            "id" => $User->guid,
+            "attributes" => $User,
+            "relationships" => [
+                "role" => [
+                    "links" => [
+                        "self" => route('user_typesGetSingleUsersType', ['typeGUID' => $User->role->guid])
+                    ],
+                    "data" => [
+                        "type" => "user_types",
+                        "guid" => $User->role->guid,
+                        "attributes" => $User->role
+                    ],
+                ],
+            ]
+        ];
+
+        $json = [
+            "data" => $data,
+            "links" => [
+                "self" => route('userGetSingleUser', ['userGUID' => $User->guid])
+            ]
+        ];
+        return $json;
     }
 
-    public function createSingle(Request $request)
+    public function createSingle(Request $request) : array
     {
+        $newUserGUID = (string)Uuid::generate(4);
 
+        $attributes = [
+            'type' => $request->type,
+            'email' => $request->email,
+            'password' => crypt($request->password),
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+
+        ];
+
+        DB::transaction(function () use ($request, $attributes, $newUserGUID) {
+
+            $attributes['guid'] = $newUserGUID;
+
+            $status = new User($attributes);
+
+            if(!$status->save())
+                throw new ValidationException($status->errors());
+
+        });
+
+        return $this->getSingle($newUserGUID);
     }
 
-    public function updateSingle(string $userGUID, Request $request)
+    public function updateSingle(string $userGUID, Request $request) : array
     {
+        $User = User::where('guid', $userGUID)->firstOrFail();
+        $UserRequest = $request->only('type', 'email', 'password', 'fname', 'lname');
 
+        if(!is_null($request->password))
+            $request->password = crypt($request->password);
+
+        if (empty($UserRequest))
+            return $this->getSingle($userGUID);
+
+        DB::transaction(function () use ($request, $User, $UserRequest) {
+
+            $User_update = $User->update($UserRequest);
+
+            if (!$User_update)
+                throw new ValidationException($User_update->errors());
+
+        });
+
+        return $this->getSingle($userGUID);
     }
 
-    public function deleteSingle(string $userGUID)
+    public function deleteSingle(string $userGUID) : array
     {
+        $User = $this->getSingle($userGUID);
 
+        DB::transaction(function () use ($userGUID) {
+            User::where('guid', $userGUID)->delete();
+        });
+
+        return $User;
     }
 
 }
